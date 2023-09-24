ebac632959a0c5992dfbac960ccc3c29caf60428
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-09-24 08:32:50 +0100

    Complete all tasks for Fundamentals Part 5 (TOP)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let newNum = 1;
+	for (let i = 0; i < power; i++) {
+		newNum *= num;
+	}
+
+	return newNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let newNum = 1;
+	for (let i = 0; i < num; num--) {
+		newNum *= num;
+	}
+
+	return newNum;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+	if (nth < 0) return "OOPS";
+	if (nth === 0) return 0;
+	
+	let firstPrevious = 1;
+	let secondPrevious = 0;
 
+	for (let i = 2; i <= nth; i++) {
+		let current = firstPrevious + secondPrevious;
+		secondPrevious = firstPrevious;
+		firstPrevious = current;
+	}
+	return firstPrevious;
 };
 
 // Do not edit below this line
